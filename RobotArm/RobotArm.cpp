bd3e15169dffde728b86e536123538f0a7452cff
Helseth <Helseth@Curt-Desktop>
2014-09-24 14:20:19 -0500

    Added variable segment lengths

--- RobotArm/RobotArm.cpp
+++ RobotArm/RobotArm.cpp
@@ -7,12 +7,13 @@ using namespace std;
 
 #define WIDTH 400
 #define HEIGHT 250
-#define SEGMENT_WIDTH 10
+#define SEGMENT_WIDTH 10.0
 #define SEGMENT_HEIGHT 4
 #define CIRCLE_RADIUS 1
 #define PI 3.14159265
 
-void recurseSegs(int, vector<int>*);
+void recurseSegs(int, vector<int>*, vector<int>*);
+void indent(int);
 
 
 int main(int argc, char* argv[]){
@@ -20,8 +21,9 @@ int main(int argc, char* argv[]){
 	int originX;
 	int originY;
 	vector<int> *angles = new vector<int>();
-	vector<int> *xCoords = new vector<int>();
-	vector<int> *yCoords = new vector<int>();
+	vector<int> *widths = new vector<int>();
+	vector<double> *xCoords = new vector<double>();
+	vector<double> *yCoords = new vector<double>();
 
 	cout << "Enter X cooridnate for first segment: ";
 	cin >> originX;
@@ -31,14 +33,24 @@ int main(int argc, char* argv[]){
 	cin >> originY;
 	yCoords->push_back(originY);
 
-	cout << "Enter rotations in degrees (360 to stop): ";
+	cout << "Enter rotations in degrees (360 to stop): \n";
+	int j = 1;
 	while(true){
+		cout << j << ": ";
 		int temp;
 		cin >> temp;
 		if (temp == 360)
 			break;
 		else
-			angles->push_back(-temp);
+			angles->push_back(temp);
+		j++;
+	}
+	cout << "Enter segment lengths: \n";
+	for (int i = 0; i < angles->size(); i++){
+		cout << i + 1 << ": ";
+		int temp;
+		cin >> temp;
+		widths->push_back(temp);
 	}
 	
 
@@ -64,48 +76,60 @@ int main(int argc, char* argv[]){
 	cout << "<rect x=\"-50\" y=\"-50\" height=\"75\" width=\"100\" stroke=\"black\" fill=\"#EEE\"/>" << endl;
 	cout << "<line x1=\"0\" y1=\"-50\" x2=\"0\" y2=\"25\" stroke=\"#888\" stroke-width=\".5\"/>" << endl;
 	cout << "<line x1=\"-50\" y1=\"0\" x2=\"50\" y2=\"0\" stroke=\"#888\" stroke-width=\".5\"/>" << endl;
-
+	int sumAngle = 0;
 	//Now the calculation code begins
-	for (int i = 1; i < angles->size(); i++){
-		xCoords->push_back(xCoords->at); //Build our coordinates here
-
+	for (int i = 0; i < angles->size(); i++){
+		sumAngle += angles->at(i);
+		xCoords->push_back(xCoords->at(i) + cos((sumAngle) * PI / 180) * widths->at(i)); //Build our coordinates here
+		yCoords->push_back(yCoords->at(i) + sin((sumAngle)* PI / 180) * widths->at(i));
 	}
 	//Build the SVG
-	recurseSegs(0, angles);
+	recurseSegs(0, angles, widths);
 	
 
 	cout << "</svg>"; //End our file
+	outFile.close(); 
+	cout.rdbuf(coutbuf); //Restore original cout
+	cout << "Coordinates for segments:\n";
+	for (int i = 0; i < xCoords->size(); i++){
+		cout << "(" << (int) xCoords->at(i) << "," << (int) yCoords->at(i) << ") ";
+	}
+	cout << endl;
+#if defined _WIN32
+	system("pause");
+#endif
 	return 0;
 }
 
-void recurseSegs(int i, vector<int> *angles){
-	if (i == angles->size())
-		//Add redbox and last dot stuff here
+void recurseSegs(int i, vector<int> *angles, vector<int> *widths){
+	if (i == angles->size()){ //This places the red box at the end
+		indent(i);
+		cout << "<g transform=\"translate(" << widths->back() << ",0)\">" << endl;
+		indent(i + 1);
+		cout << "<use xlink:href=\"#dot\" />" << endl;
+		indent(i + 1);
+		cout << "<use xlink:href=\"#redbox\" />" << endl;
+		indent(i);
+		cout << "</g>" << endl;
 		return;
+	}
 		
 
-	for (int j = 0; j < i; j++){ //Indenting
-		cout << "\t";
-	}
+	indent(i);
 	if (i != 0)
-		cout << "<g transform=\"translate(" << SEGMENT_WIDTH << ",0) rotate(" << angles->at(i) << ")\">" << endl; //We want to translate and rotate relative to the previous segment
+		cout << "<g transform=\"translate(" << widths->at(i - 1) << ",0) rotate(" << angles->at(i) << ")\">" << endl; //We want to translate and rotate relative to the previous segment
 	else
-		cout << "<g transform=\"rotate(" << angles->at(i) << ")\">" << endl; //This is for the first box
-
-	for (int j = 0; j < i + 1; j++){
-		cout << "\t";
-	}
-	cout << "<use xlink:href=\"#segment\"/>" << endl;
-
-	for (int j = 0; j < i + 1; j++){
-		cout << "\t";
-	}
-
+		cout << "<g transform=\"scale(1,-1) rotate(" << angles->at(i) << ")\">" << endl; //This is for the first box scale Y values by -1 so we follow the unit circle
+	indent(i + 1);
+	cout << "<use xlink:href=\"#segment\" transform=\"scale(" << widths->at(i) / SEGMENT_WIDTH << ", 1)\"/>" << endl;
+	indent(i + 1);
 	cout << "<use xlink:href=\"#dot\"/>" << endl;
-	recurseSegs(i + 1, angles);
-
-	for (int j = 0; j < i; j++){
-		cout << "\t";
-	}
+	recurseSegs(i + 1, angles, widths);
+	indent(i);
 	cout << "</g>" << endl;
+}
+
+void indent(int i){
+	for (int j = 0; j < i; j++)
+		cout << "\t";
 }
